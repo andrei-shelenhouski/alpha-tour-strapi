ba18b7b280f07d69253f1d762a80397ec14da121
Andrei Shelenhouski <andrei.shelenhouski@gmail.com>
2025-09-25 11:27:32 +0200

    fix: restructure strapi::cors middleware configuration for clarity

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -31,7 +31,22 @@ export default [
       },
     },
   },
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "http://localhost:1337",
+        "http://localhost:3000",
+        "https://market-assets.strapi.io",
+        "https://images.ctfassets.net",
+        "https://belavia.by",
+        process.env.BUNNY_PULL_ZONE,
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
