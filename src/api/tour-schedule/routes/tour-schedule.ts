b1fd8980c9d99fb4601c86fddd043c3515197698
Andrei Shelenhouski <andrei.shelenhouski@gmail.com>
2026-07-22 20:19:53 +0200

    feat: add tour-schedule content type with attributes and relations

---
+++ src/api/tour-schedule/routes/tour-schedule.ts
@@ -0,0 +1,7 @@
+/**
+ * tour-schedule router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::tour-schedule.tour-schedule');
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1070,6 +1070,55 @@ export interface ApiRoomTypeRoomType extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiTourScheduleTourSchedule
+  extends Struct.CollectionTypeSchema {
+  collectionName: 'tour_schedules';
+  info: {
+    displayName: 'Tour Schedule';
+    pluralName: 'tour-schedules';
+    singularName: 'tour-schedule';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    currency: Schema.Attribute.Enumeration<
+      ['PLN', 'EUR', 'USD', 'BYN', 'GEL']
+    > &
+      Schema.Attribute.DefaultTo<'BYN'>;
+    date: Schema.Attribute.Date;
+    day_1: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    day_2: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    day_3: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    day_4: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    day_5: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    day_6: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    day_7: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    effective_date: Schema.Attribute.Date;
+    expiry_date: Schema.Attribute.Date;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::tour-schedule.tour-schedule'
+    > &
+      Schema.Attribute.Private;
+    page: Schema.Attribute.Relation<'manyToOne', 'api::universal.universal'>;
+    price: Schema.Attribute.Decimal;
+    publishedAt: Schema.Attribute.DateTime;
+    schedule_type: Schema.Attribute.Enumeration<
+      ['fixed_date', 'weekly', 'on_request']
+    > &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'fixed_date'>;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface ApiTourTypeTourType extends Struct.CollectionTypeSchema {
   collectionName: 'tour_types';
   info: {
@@ -1371,6 +1420,10 @@ export interface ApiUniversalUniversal extends Struct.CollectionTypeSchema {
     >;
     posts: Schema.Attribute.Relation<'oneToMany', 'api::post.post'>;
     publishedAt: Schema.Attribute.DateTime;
+    schedules: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::tour-schedule.tour-schedule'
+    >;
     seo: Schema.Attribute.Component<'shared.seo', false> &
       Schema.Attribute.SetPluginOptions<{
         i18n: {
@@ -1941,6 +1994,7 @@ declare module '@strapi/strapi' {
       'api::provider.provider': ApiProviderProvider;
       'api::room-category.room-category': ApiRoomCategoryRoomCategory;
       'api::room-type.room-type': ApiRoomTypeRoomType;
+      'api::tour-schedule.tour-schedule': ApiTourScheduleTourSchedule;
       'api::tour-type.tour-type': ApiTourTypeTourType;
       'api::tour.tour': ApiTourTour;
       'api::transport-type.transport-type': ApiTransportTypeTransportType;
